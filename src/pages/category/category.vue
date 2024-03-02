440fb7783603e2a8b4f34099dc80dd8d607fd344
ice_wu <xuhong267@gmail.com>
2024-03-02 21:31:43 +0800

    add feature/home

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,6 +1,79 @@
 <template>
-  <view>
-    <text>category</text>
+  <view class="category">
+    <view class="category-container flex">
+      <!-- 左侧导航 -->
+      <scroll-view class="left" scroll-y show-scrollbar=true>
+        <view class="list-item">
+          <view class="nav-item active">水果第一</view>
+        
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果水果</view>
+          <view class="nav-item">水果最后</view>
+        </view>
+      </scroll-view>
+      <!-- 右侧列表 -->
+      <view class="right">right</view>
+      
+    </view>
   </view>
 </template>
 
@@ -12,4 +85,40 @@ name:'CategoryIndex'
 
 <style lang="less" scoped>
 
+.category{
+height: 100%;
+}
+.category-container{
+height: 100%;
+}
+.left{
+  
+  width: 160rpx;
+  height: 100%;
+  background-color: aliceblue;
+  & ::-webkit-scrollbar {
+  display: none;
+  }
+  .nav-item{
+    position: relative;
+    width: 100%;
+    height: 72rpx;
+    line-height: 72rpx;
+    text-align: center;
+    font-size: 26rpx;
+    &.active{
+      color: rgb(255, 140, 0);
+      background-color: #fff;
+      &::before{
+        position: absolute;
+        left:5rpx;
+        top: 15%;
+        content: '';
+        width: 10rpx;
+        height: 70%;
+        background-color: aqua;
+      }
+    }
+  }
+}
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,43 +1,64 @@
 <template>
-  <view class="content">
-    <view class="uni-margin-wrap">
-      <swiper
-        class="swiper"
-        circular
-        :indicator-dots="indicatorDots"
-        :autoplay="autoplay"
-        :interval="interval"
-        :duration="duration"
-      >
-        <swiper-item v-for="b in bannerList" :key="b.id">
-          <image class="image" :src="b.imageUrl" mode="aspectFill"></image>
-        </swiper-item>
-      </swiper>
-    </view>
-    <view class="nav-list flex">
-      <view class="nav-list-item flex-c"  v-for="(c1,index) in category1" :key="c1.id" >
-        <image class="nav-item-image" :class="{small:index>4}" :src="c1.imageUrl" mode="aspectFill"></image>
-        <text>{{c1.name}}</text>
-      </view>
-    </view>
-		<view class="goodslist">
-			<GoodsList></GoodsList>
+	<view class="container">
+		<view class="content">
+			<view class="uni-margin-wrap">
+				<swiper
+					class="swiper"
+					circular
+					:indicator-dots="indicatorDots"
+					:autoplay="autoplay"
+					:interval="interval"
+					:duration="duration"
+				>
+					<swiper-item v-for="b in bannerList" :key="b.id">
+						<view class="swiper-item" >
+							<image class="image" :src="b.imageUrl" mode="aspectFill"></image>
+						</view>
+					</swiper-item>
+				</swiper>
+			</view>
+			<view class="nav-list flex">
+				<view class="nav-list-item flex-c"  v-for="(c1,index) in category1" :key="c1.id" >
+					<image class="nav-item-image" :class="{small:index>4}" :src="c1.imageUrl" mode="aspectFill"></image>
+					<text>{{c1.name}}</text>
+				</view>
+			</view>
+			<view class="link">
+				<image mode="widthFix"  src="https://img.tukuppt.com/photo-big/17/97/44/886545fa13bb3db455.jpg!/fh/350" class="image"></image>
+			</view>
+			<view class="goodslist">
+				<GoodsList title="猜你喜欢">
+					<view  v-for="item in likeListGoods" :key="item.id">
+					<GoodsCard :goodsitem="item"></GoodsCard>
+					</view>
+				</GoodsList>
+				<GoodsList title="人气推荐">
+					<view  v-for="item in recommondGoods" :key="item.id">
+					<GoodsCard :goodsitem="item"></GoodsCard>
+					</view>
+				</GoodsList>
+			</view>
 		</view>
-  </view>
+	</view>
+ 
 </template>
 
 <script>
-import { getBanner, findCategory1 } from "@/api/category.js";
+import { getBanner, findCategory1,findListGoods,findRecommendGoods } from "@/api/category.js";
 import GoodsList from'@/components/goodslist/goodslist.vue'
+import GoodsCard from'@/components/goodscard/goodscard.vue'
 export default {
   name: "Index",
 	components:{
-		GoodsList
+		GoodsList,
+		GoodsCard
 	},
   data() {
     return {
       bannerList: [],
       category1: [],
+			likeListGoods:[],
+			recommondGoods:[],
       title: "Hello",
       background: ["color1", "color2", "color3"],
       indicatorDots: true,
@@ -49,6 +70,8 @@ export default {
   onLoad() {
     this.getBannerList();
 		this.getCategory1()
+		this.getlikeListGoods()
+		this.getRecommendGoods()
   },
   methods: {
     changeIndicatorDots(e) {
@@ -67,16 +90,35 @@ export default {
     async getCategory1() {
       try {
         const res =await findCategory1();
-console.log(res);
+// console.log(res);
         this.category1 = res;
       } catch (error) {
         console.log("获取一级分类失败");
+      }
+    },
+		async getlikeListGoods() {
+      try {
+        const res =await findListGoods();
+// console.log(res);
+        this.likeListGoods = res;
+      } catch (error) {
+        console.log("获取like失败");
+      }
+    },
+		async getRecommendGoods() {
+      try {
+        const res =await findRecommendGoods();
+console.log(res);
+        this.recommondGoods = res;
+      } catch (error) {
+        console.log("获取recomm失败");
       }
     },
     async getBannerList() {
       try {
         const res = await getBanner();
         this.bannerList = res;
+				console.log(this.bannerList);
         // console.log(res);
         // const [err,res] = await uni.request({
         // 	url:`/api/mall-api/index/findBanner`
@@ -96,12 +138,21 @@ console.log(res);
 
 <style lang="less">
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	width: 100%;
+	height: 100%;
+
+	.link{
+		text-align: center;
+		padding: 10rpx 0;
+		background-color: #eee;
+		.image{
+			width: 700rpx;
+
+		}
+	}
 .goodslist{
 	width: 100%;
+	padding-bottom: 50rpx;
 }
 	.nav-list{
 		font-size: 20rpx;
@@ -130,7 +181,7 @@ console.log(res);
 	}
 }
 .uni-margin-wrap {
-  width: 690rpx;
+	height: 345rpx;
   width: 100%;
 }
 .swiper {
